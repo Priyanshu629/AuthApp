be0b31cd77531531014ebece1f73256cb0f36364
Priyanshu kumar Mishra <104246169+Priyanshu629@users.noreply.github.com>
2024-01-08 07:03:51 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,29 +1,29 @@
 import  { useEffect,useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 const Home = () => {
-    const [login,setLogin]=useState(true)
+    
     const navigate=useNavigate()
     let user=localStorage.getItem('loggedin')
     let email=localStorage.getItem('email')
     let username=localStorage.getItem('username')
 
-    if(!user){
-       setLogin(false)
-    }
+ 
     const logout=()=>{
       localStorage.clear()
       navigate('/')
     }
    
-   
-   login?
-   (
+      if(!user){
+       return window.location.href='/'
+    }
+
+  return (
     <div className='p-3'>
      <h2>Welcome {username}</h2> 
      <h2>Your Email:{email}</h2>
      <button className='btn btn-danger' onClick={logout}>Logout</button>
     </div>
-  ) : window.location.href='/'
+  ) 
     
 }
 
